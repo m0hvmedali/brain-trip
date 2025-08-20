19a1ab59cdf309c5a7600f9bf082a9f229490bbf
Mohamed Ali <mohamedalix546@gmail.com>
2025-08-20 23:28:54 +0300

    solve proplem 1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 export default defineConfig({
   plugins: [react()],
+  base: './', // مهم جداً عشان يحل مشكلة المسارات
   resolve: {
     alias: { 
       '@': path.resolve(__dirname, './src'),
